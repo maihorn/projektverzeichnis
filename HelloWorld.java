d748683bbf4a2b3c5d2423197b869c5ceeb6b48e
Maikel Horn <horn.maikel@gmail.com>
2020-12-11 22:43:01 +0100

    new files

---
+++ HelloWorld.java
@@ -0,0 +1,24 @@
+package de.maikel;
+
+public class HelloWorld {
+
+	public void aufruf() {
+		// TODO Auto-generated method stub
+
+		// Ausgabe Hello World
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+		System.out.println("Hello World!");
+
+		HelloWorld ausgabe = new HelloWorld();
+		ausgabe.helloProgrammer();
+		
+	}
+	
+	public void helloProgrammer() {
+		System.out.println("Hallo Programmierer!");
+		System.out.println("Programmieren mit Java");
+		System.out.println("ist toll!");
+	}
+
+}
